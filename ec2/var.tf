2226dbc396f15959eaef94be178b8dafc022fd0b
Priyadarshini Itraj <50285249+itrajp@users.noreply.github.com>
2020-07-23 23:26:28 +0530

    Add files via upload

---
+++ ec2/var.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION"{
+    default = "us-east-1"
+}
+variable "AMIS"{
+    type = map(string)
+    default ={
+        us-east-1 = "ami-09d95fab7fff3776c"
+    }
+}
+variable "INST_TYPE"{
+    default = "t2.micro"
+}
